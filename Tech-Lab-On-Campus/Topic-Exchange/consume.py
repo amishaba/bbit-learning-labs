9c23ccd94fd1f444fdab81b7be06d507dab320c7
amishaba <amishaba@usc.edu>
2024-03-01 16:12:40 -0800

    consumer exchange

--- Tech-Lab-On-Campus/Topic-Exchange/consume.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consume.py
@@ -19,12 +19,14 @@ from solution.consumer_sol import mqConsumer  # pylint: disable=import-error
 
 def main(sector: str, queueName: str) -> None:
 
-    bindingKey = sector + ".info"
+    bindingKey = "*." + sector + ".*"
     
     # Implement Logic to Create Binding Key from the ticker and sector variable -  Step 2
     #
     #                       WRITE CODE HERE!!!
     #
+
+
     
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
@@ -36,6 +38,8 @@ if __name__ == "__main__":
     sector = sys.argv[1]
 
     queue = sys.argv[2]
+
+    
 
     # Implement Logic to read the sector and queueName string from the command line and save them - Step 1
     #
